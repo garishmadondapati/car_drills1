b3b346b8a0021147e5be031fc7bbdbda738c26fb
garishma <garishmadondapati@gmail.com>
2024-02-22 18:26:52 +0530

    finding car with id

--- problem-1.js
+++ problem-1.js
@@ -2,7 +2,7 @@
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by calling a function that will return the data for that car. Then log the car's year, make, and model in the console log in the format of:
 "Car 33 is a *car year goes here* *car make goes here* *car model goes here*"
 
-let inventory = require('./data')
+//let inventory = require('./data')
 
 function findOutCarWithId(data,id){
     for(let i=0;i<data.length;i++){
@@ -11,5 +11,7 @@ function findOutCarWithId(data,id){
         }
     }
 }
-let result = findOutCarWithId(inventory,33);
-console.log(`car 33 is a ${result.car_year}  ${result.car_make}  ${result.car_model}`)
+//let result = findOutCarWithId(inventory,33);
+//console.log(`car 33 is a ${result.car_year}  ${result.car_make}  ${result.car_model}`)
+
+module.exports=findOutCarWithId
