b25a8ae9679fb7fe0342a12068415ddcd77a0c68
garishma <garishmadondapati@gmail.com>
2024-02-22 19:31:52 +0530

    problem3 modified

--- problem-3.js
+++ problem-3.js
@@ -2,10 +2,11 @@ let inventory=require('./data.js')
 
 function carModels(arr){
     if(Array.isArray(arr)){
-    let car_models=[];
-    for(let car of arr){
-        car_models.push(car.car_model);
-    }
+    let car_models=arr.map((element)=>{
+        return element.car_model;
+    })
+    
+    
     car_models.sort();
     return car_models
 }
