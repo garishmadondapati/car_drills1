e974a7b402ca9d3f69fbd5caefe4a89132cbc77a
garishma <garishmadondapati@gmail.com>
2024-02-22 18:32:51 +0530

    finding car models in sorting

---
+++ problem-3.js
@@ -0,0 +1,16 @@
+let inventory=require('./data.js')
+
+function carModels(arr){
+    if(Array.isArray(arr)){
+    let car_models=[];
+    for(let car of arr){
+        car_models.push(car.car_model);
+    }
+    car_models.sort();
+    return car_models
+}
+};
+
+module.exports=carModels;
+
+console.log(carModels(inventory));
