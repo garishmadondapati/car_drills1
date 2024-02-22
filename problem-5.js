3f78c16fbb967cdeca4c629e503b55c86b25e86f
garishma <garishmadondapati@gmail.com>
2024-02-22 18:36:25 +0530

    finding car years less than 2000

--- problem-5.js
+++ problem-5.js
@@ -1,7 +1,7 @@
 // ==== Problem #5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the array you just obtained from the previous problem, find out how many cars were made before the year 2000 and return the array of older cars and log its length.
 
-let inventory = require('./data')
+//let inventory = require('./data')
 
 function carYears(data){
     let newArray =[]
@@ -12,5 +12,7 @@ function carYears(data){
     }
     return newArray
 }
-let carYearsnewArray= carYears(inventory)
-console.log(carYearsnewArray)
+//let carYearsnewArray= carYears(inventory)
+//console.log(carYearsnewArray)
+
+module.exports =carYears
